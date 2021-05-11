f233644f0b18d249296ef9e10a948e8eee70b1cd
osc8812 <82039033+osc8812@users.noreply.github.com>
2021-05-11 14:54:43 +0200

    Klar

--- src/main/java/Google.java
+++ src/main/java/Google.java
@@ -25,11 +25,8 @@ public class Google {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("https://www.google.com/");
-        // identify element
         WebElement p = driver.findElement(By.name("q"));
-        //enter text with sendKeys() then apply submit()
         p.sendKeys("Software Testing");
-        // Explicit wait condition for search results
         WebDriverWait w = new WebDriverWait(driver, 5);
         w.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ul")));
         p.submit();
